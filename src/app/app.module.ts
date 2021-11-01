5a94880a762fad58ee263df7d74fa2cd36f9614c
Siddhansh Jatale <sjatale2@gmail.com>
2021-11-01 18:46:38 +0530

    firebase create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,17 @@ import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ManagerComponent } from './manager/manager.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -36,9 +47,21 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     MatButtonModule,
     MatExpansionModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'document-manager-73275',
+    appId: '1:328646998046:web:6e9d162ef44f911e7dd60f',
+    storageBucket: 'document-manager-73275.appspot.com',
+    apiKey: 'AIzaSyDL8HcrvubICxcQFVpP-qWji2LSO7PWMyk',
+    authDomain: 'document-manager-73275.firebaseapp.com',
+    messagingSenderId: '328646998046',
+    measurementId: 'G-S73N7P1EE6',
+  },
   production: true
 };
