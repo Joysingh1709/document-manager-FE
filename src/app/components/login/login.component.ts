93996b88d705806b64734f03b35df94be97654f4
Siddhansh Jatale <sjatale2@gmail.com>
2021-11-03 02:26:55 +0530

    login changes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required])
+    })
+  }
+
+  onLogin() {
+    if (!this.loginForm.valid) {
+      console.log("please check values")
+    }
+    else {
+      console.log(this.loginForm.value)
+    }
+  }
+
+  onGoogleLogin(){
+    
   }
 
 }
