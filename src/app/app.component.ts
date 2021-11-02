2f542ccd2117f59b40dce34eaebd6556526fb53e
Joy Singh <avnishmamta060606@gmail.com>
2021-11-03 01:34:47 +0530

    login component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from './services/firebase.service';
+import { Auth } from '@angular/fire/auth'
 @Component({
   selector: 'app-component',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'documentManager';
+
+  fAuth: Auth | undefined;
+
+  constructor(private firebaseService: FirebaseService) {
+
+  }
+
+  ngOnInit(): void { // lifecycle hook
+    this.firebaseService.user.subscribe((value) => {
+      // do something on user logged in or else logout
+    });
+
+    this.fAuth?.onAuthStateChanged((user) => {
+      if (user) {
+        console.log("user is logged in");
+      }
+      else {
+        console.log("user is logged out");
+      }
+    });
+  }
 }
